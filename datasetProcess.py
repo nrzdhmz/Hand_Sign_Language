461b3eb57351218a0cb9988c9b295ce799ac320e
Hamza <nuruzadahamza@gmail.com>
2025-08-12 18:43:31 +0400

    data collection is done

--- datasetProcess.py
+++ datasetProcess.py
@@ -1,11 +1,17 @@
 import cv2
 import numpy as np
+import os
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 offset = 20
 fixed_size = 400
+
+folder = "Data/DELETE"
+os.makedirs(folder, exist_ok=True) 
+
+img_counter = 0 
 
 while True:
     success, img = cap.read()
@@ -28,7 +34,7 @@ while True:
         imgCrop = img[y1:y2, x1:x2]
 
         red_bg = np.zeros((fixed_size, fixed_size, 3), np.uint8)
-        red_bg[:] = (0, 0, 255)
+        red_bg[:] = (247, 24, 244)
 
         h_crop, w_crop = imgCrop.shape[:2]
 
@@ -44,8 +50,16 @@ while True:
         cv2.imshow("imgCrop", red_bg)
 
     cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == 27:
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == 27:  
         break
+    elif key == ord('s'): 
+        if hands:
+            img_name = f"{folder}/img_{img_counter}.jpg"
+            cv2.imwrite(img_name, red_bg)
+            print(f"Saved {img_name}")
+            img_counter += 1
 
 cap.release()
 cv2.destroyAllWindows()
